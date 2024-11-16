76b1c02469cbb0b733f584802ffde56a49bd8016
itsmerit37 <itsmerit37@gmail.com>
2024-11-16 22:18:05 +0530

    String

---
+++ basic/05_string.js
@@ -0,0 +1,20 @@
+const name ="Ritesh-working-in-infosys"
+const repoCount=50
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+//run this in console u will get all types of String nd its method
+
+
+const gameName=new String("Ritesh")
+//console.log(gameName);
+
+//console.log(gameName.charAt(1));
+
+const anotherString=gameName.slice(-6,3)
+console.log(anotherString);
+console.log(name.split('-'));
+
+
+
+
--- basic/dataTypes-summary.js
+++ basic/dataTypes-summary.js
@@ -24,7 +24,10 @@
         const anotherId=Symbol('123')
         console.log(id===anotherId); //false
 
+
         const heros=["shaktiman","naagraj","doga"];
+
+
         let myObj=
         {
              name:"Ritesh",
@@ -36,5 +39,32 @@
             console.log("Hello");
             
         }
+
         
+
+    //--------------------------------------Mmemory-------------------------------------------------
+    //Stack (primitive) heap (non-primitive)
+
+    let myOriginalName="Ritesh Singh"
+    let anotherName=myOriginalName
+    anotherName="Rishu Singh"
+    console.log(myOriginalName);
+    console.log(anotherName);
+
+    let userOne=
+    {
+        name:"Ritesh",
+        upi:"916167@ybl"
+    }
+    let userTwo=userOne
+
+    userTwo.upi="8795@ybl"
+    console.log(userOne.upi);
+    console.log(userTwo.upi);
+    
+    
+    
+    
+
+
         
