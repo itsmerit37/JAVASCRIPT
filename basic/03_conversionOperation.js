60262abee54ab4e0c09ab74b402cac45d13a579d
itsmerit37 <itsmerit37@gmail.com>
2024-11-16 16:17:12 +0530

    conversionOperation

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,50 @@
+let score="33"
+let valueInNumber=Number(score)
+
+// console.log(typeof score);
+
+
+// console.log(typeof valueInNumber);
+
+/* 
+"33" =>
+    "33abc" => NaN
+true =>1 false 0
+ */
+
+let isLoggedIn=0
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+// 
+
+
+/* 
+true =>1 false => 0
+"" =>false
+"Ritesh" => true
+ */
+
+
+
+//----------------------Operations ------------------------------------
+
+let value =3
+let negValue =-value
+//console.log(negValue);
+
+let str1= "hello"
+let str2= "Ritesh"
+let str3= str1+" "+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1 + 2 + "2");
+console.log(1 + 2 + "2" +"2" +2);
+
+
+
+
+
+
+
