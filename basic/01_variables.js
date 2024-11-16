37e12c469129228d0106098f3950542eaeabdefa
itsmerit37 <itsmerit37@gmail.com>
2024-11-16 12:08:06 +0530

    dataTypes

---
+++ basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId= 144553
+let accountEmail= "itsmerit37@gmail.com"
+
+/* var accountPassword="12345"
+
+prefer not to use var in block scope and functional scope
+*/
+
+accountCity= "UP"
+
+// accountId=2 not allowed 
+accountEmail="xyz@gmail.com"
+
+ accountPassword="1222"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+console.log(typeof(accountCity));
---
+++ basic/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";// treat all JS code as a newer version
+
+//alert("hello")  we are using node js ,not browser
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+let name="Ritesh";
+let age=26
+let isLoggedIn= false
+let state
+
+console.log(typeof name);
+
+
+/*
+types of data type--------
+
+number
+bigint
+string=> ""
+boolean=> true amd false
+null => standalone value
+undefined =>
+symbol
+
+
+*/
+
