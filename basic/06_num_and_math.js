75bb01b9f10ec4ba986ffe87a718583e12006002
itsmerit37 <itsmerit37@gmail.com>
2024-11-16 22:44:36 +0530

    num_and_math

---
+++ basic/06_num_and_math.js
@@ -0,0 +1,33 @@
+const score= new Number(100)
+console.log(score);
+console.log(score.toString());
+
+
+const number=100
+console.log(number);
+
+const hund=100000
+console.log(hund.toLocaleString('en-IN'));
+
+
+
+//+++++++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.8));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
